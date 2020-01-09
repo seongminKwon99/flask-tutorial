b88ba0d9c750d5d6f08fa2b7f6d135dee4af5ff9
seongminKwon99 <59546989+seongminKwon99@users.noreply.github.com>
2020-01-09 15:17:53 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -11,5 +11,9 @@ def hello_world():
 def hello_world2():
     return "Hello, Sungshin2!"
 
+@app.route("/bye")
+def hello_world3):
+    return "bye- bye
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
